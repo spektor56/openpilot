038448c2e23490e008568e841e7aa158c753681e
Brett Sanderson <l-brett-l@hotmail.com>
2021-03-10 17:06:54 -0500

    add brake logic back

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -344,7 +344,11 @@ class CarState(CarStateBase):
       if self.prev_cruise_buttons != 2: #cancel
         if self.cruise_buttons == 2:
           self.accEnabled = False   
+      if ret.brakePressed:
+        self.accEnabled = False
       ret.cruiseState.enabled = self.accEnabled
+
+      
 
     # TODO: discover the CAN msg that has the imperial unit bit for all other cars
     self.is_metric = not cp.vl["HUD_SETTING"]['IMPERIAL_UNIT'] if self.CP.carFingerprint in (CAR.CIVIC) else False
